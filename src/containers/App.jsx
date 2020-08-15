2630725b1d3eba9911302e41fb98c1970bd29011
Jesús Montero <jesusmontero.developer@gmail.com>
2020-08-15 16:41:19 -0500

    Add images

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,6 +5,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
@@ -13,7 +14,7 @@ const App = () => (
     <Header />
     <Search />
 
-    <Categories>
+    <Categories title='My list'>
       <Carousel>
         <CarouselItem />
         <CarouselItem />
@@ -21,6 +22,26 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Categories title='Trends'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title='Originals'>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Footer />
   </div>
 );
 export default App;
